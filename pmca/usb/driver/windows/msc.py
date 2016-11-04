8d37f43da5f540a955d2c7501cf96b9077ee0732
ma1co <ma1co@users.noreply.github.com>
2016-11-04 17:04:12 +0000

    Windows: Use Setup API to list MSC devices

--- pmca/usb/driver/windows/msc.py
+++ pmca/usb/driver/windows/msc.py
@@ -1,13 +1,17 @@
 """A wrapper to use IOCTL_SCSI_PASS_THROUGH_DIRECT"""
 
-from comtypes.client import *
+from comtypes import GUID
 from ctypes import *
+from ctypes.wintypes import *
+import string
+import sys
 from win32file import *
 
 from . import *
 from .. import *
 
 IOCTL_SCSI_PASS_THROUGH_DIRECT = 0x4d014
+IOCTL_STORAGE_GET_DEVICE_NUMBER = 0x2d1080
 
 SCSI_IOCTL_DATA_OUT = 0
 SCSI_IOCTL_DATA_IN = 1
@@ -37,26 +41,125 @@ class SCSI_PASS_THROUGH_DIRECT_WITH_BUFFER(Structure):
   ('ucSenseBuf', c_ubyte * 32),
  ]
 
+class SP_DEVICE_INTERFACE_DATA(Structure):
+ _fields_ = [
+  ('cbSize', DWORD),
+  ('InterfaceClassGuid', GUID),
+  ('Flags', DWORD),
+  ('Reserved', POINTER(ULONG)),
+ ]
+
+class SP_DEVICE_INTERFACE_DETAIL_DATA(Structure):
+ _pack_ = 4 if sys.maxsize > 2**32 else 2
+ _fields_ = [
+  ('cbSize', DWORD),
+  ('DevicePath', WCHAR * 1),
+ ]
+
+class SP_DEVINFO_DATA(Structure):
+ _fields_ = [
+  ('cbSize', DWORD),
+  ('ClassGuid', GUID),
+  ('DevInst', DWORD),
+  ('Reserved', POINTER(ULONG)),
+ ]
+
+class STORAGE_DEVICE_NUMBER(Structure):
+ _fields_ = [
+  ('DeviceType', DWORD),
+  ('DeviceNumber', ULONG),
+  ('PartitionNumber', ULONG),
+ ]
+
+SetupDiGetClassDevs = windll.setupapi.SetupDiGetClassDevsW
+SetupDiGetClassDevs.restype = HANDLE
+SetupDiGetClassDevs.argtypes = [POINTER(GUID), LPCWSTR, HWND, DWORD]
+
+SetupDiDestroyDeviceInfoList = windll.setupapi.SetupDiDestroyDeviceInfoList
+SetupDiDestroyDeviceInfoList.restype = BOOL
+SetupDiDestroyDeviceInfoList.argtypes = [HANDLE]
+
+SetupDiEnumDeviceInterfaces = windll.setupapi.SetupDiEnumDeviceInterfaces
+SetupDiEnumDeviceInterfaces.restype = BOOL
+SetupDiEnumDeviceInterfaces.argtypes = [HANDLE, c_void_p, POINTER(GUID), DWORD, POINTER(SP_DEVICE_INTERFACE_DATA)]
+
+SetupDiGetDeviceInterfaceDetail = windll.setupapi.SetupDiGetDeviceInterfaceDetailW
+SetupDiGetDeviceInterfaceDetail.restype = BOOL
+SetupDiGetDeviceInterfaceDetail.argtypes = [HANDLE, POINTER(SP_DEVICE_INTERFACE_DATA), POINTER(SP_DEVICE_INTERFACE_DETAIL_DATA), DWORD, POINTER(DWORD), POINTER(SP_DEVINFO_DATA)]
+
+CM_Get_Child = windll.CfgMgr32.CM_Get_Child
+CM_Get_Child.restype = DWORD
+CM_Get_Child.argtypes = [POINTER(DWORD), DWORD, ULONG]
+
+CM_Get_Sibling = windll.CfgMgr32.CM_Get_Sibling
+CM_Get_Sibling.restype = DWORD
+CM_Get_Sibling.argtypes = [POINTER(DWORD), DWORD, ULONG]
+
+GUID_DEVINTERFACE_USB_DEVICE = GUID('{A5DCBF10-6530-11D2-901F-00C04FB951ED}')
+GUID_DEVINTERFACE_DISK = GUID('{53F56307-B6BF-11D0-94F2-00A0C91EFB8B}')
+DIGCF_PRESENT = 2
+DIGCF_DEVICEINTERFACE = 16
+
+def _listDeviceClass(guid):
+ handle = SetupDiGetClassDevs(byref(guid), None, None, DIGCF_DEVICEINTERFACE | DIGCF_PRESENT)
+ if handle == INVALID_HANDLE_VALUE:
+  raise Exception('SetupDiGetClassDevs failed')
+
+ i = 0
+ interfaceData = SP_DEVICE_INTERFACE_DATA()
+ interfaceData.cbSize = sizeof(SP_DEVICE_INTERFACE_DATA)
+ while SetupDiEnumDeviceInterfaces(handle, None, byref(guid), i, byref(interfaceData)):
+  size = c_ulong(0)
+  SetupDiGetDeviceInterfaceDetail(handle, byref(interfaceData), None, 0, byref(size), None)
+
+  interfaceDetailData = SP_DEVICE_INTERFACE_DETAIL_DATA()
+  interfaceDetailData.cbSize = sizeof(SP_DEVICE_INTERFACE_DETAIL_DATA)
+  resize(interfaceDetailData, size.value)
+  devInfoData = SP_DEVINFO_DATA()
+  devInfoData.cbSize = sizeof(SP_DEVINFO_DATA)
+  if not SetupDiGetDeviceInterfaceDetail(handle, byref(interfaceData), byref(interfaceDetailData), size, None, byref(devInfoData)):
+   raise Exception('SetupDiGetDeviceInterfaceDetail failed')
+
+  yield devInfoData.DevInst, wstring_at(byref(interfaceDetailData, SP_DEVICE_INTERFACE_DETAIL_DATA.DevicePath.offset))
+  i += 1
+
+ if not SetupDiDestroyDeviceInfoList(handle):
+  raise Exception('SetupDiDestroyDeviceInfoList failed')
+
+def _listDeviceChildren(inst):
+ child = DWORD(inst)
+ f = CM_Get_Child
+ while not f(byref(child), child, 0):
+  yield child.value
+  f = CM_Get_Sibling
+
+def _listLogicalDrives(type=DRIVE_REMOVABLE):
+ mask = GetLogicalDrives()
+ for i, l in enumerate(string.ascii_uppercase):
+  if mask & (1 << i) and GetDriveType('%s:\\' % l) == type:
+   yield '\\\\.\\%s:' % l
+
+def _getStorageNumber(path):
+ handle = CreateFile(path, 0, FILE_SHARE_READ | FILE_SHARE_WRITE, None, OPEN_EXISTING, 0, None)
+ deviceNumber = STORAGE_DEVICE_NUMBER()
+ DeviceIoControl(handle, IOCTL_STORAGE_GET_DEVICE_NUMBER, None, deviceNumber)
+ CloseHandle(handle)
+ return deviceNumber.DeviceType, deviceNumber.DeviceNumber
 
 def listDevices():
  """Lists all detected mass storage devices"""
- # Connect to a WMI object
- wmi = CoGetObject('winmgmts:')
-
- def getUsbDrivesFromSerial(serial):
-  for disk in wmi.InstancesOf('Win32_DiskDrive'):
-   if disk.Properties_['InterfaceType'].Value == 'USB' and disk.Properties_['SerialNumber'].Value == serial:
-    for partition in wmi.AssociatorsOf(disk.Path_.Path, '', 'Win32_DiskPartition'):
-     for logicalDisk in wmi.AssociatorsOf(partition.Path_.Path, '', 'Win32_LogicalDisk'):
-      yield logicalDisk
-
- for hub in wmi.InstancesOf('Win32_USBHub'):
-  deviceId = hub.Properties_['DeviceID'].Value
-  idVendor, idProduct = parseDeviceId(deviceId)
-  serial = deviceId.rpartition('\\')[2]
-  drive = next(getUsbDrivesFromSerial(serial), None)
-  if drive:
-   yield UsbDevice(drive.Properties_['DeviceID'].Value, idVendor, idProduct, USB_CLASS_MSC)
+ # Similar to what calibre does: https://github.com/kovidgoyal/calibre/blob/master/src/calibre/devices/winusb.py
+ logicalDrives = dict((_getStorageNumber(l), l) for l in _listLogicalDrives())
+ disks = dict(_listDeviceClass(GUID_DEVINTERFACE_DISK))
+ usbDevices = dict(_listDeviceClass(GUID_DEVINTERFACE_USB_DEVICE))
+ for usbInst, usbPath in usbDevices.iteritems():
+  for diskInst in _listDeviceChildren(usbInst):
+   if diskInst in disks:
+    storageNumber = _getStorageNumber(disks[diskInst])
+    if storageNumber in logicalDrives:
+     idVendor, idProduct = parseDeviceId(usbPath)
+     yield UsbDevice(logicalDrives[storageNumber], idVendor, idProduct, USB_CLASS_MSC)
+     break# only return the first disk for every device
 
 
 class MscDriver:
