73be1df4f9c8f1e5632462e08895bbdceda2b68b
ma1co <ma1co@users.noreply.github.com>
2021-08-11 10:47:31 +0000

    Clean up http util

--- pmca/util/http.py
+++ pmca/util/http.py
@@ -1,37 +1,17 @@
 """Some methods to make HTTP requests"""
 
 from collections import namedtuple
-import random
-import string
 
 try:
  from urllib.parse import *
  from urllib.request import *
- from http.cookiejar import *
 except ImportError:
  # Python 2
  from urllib import *
  from urllib2 import *
- from cookielib import *
  from urlparse import *
 
-HttpResponse = namedtuple('HttpResponse', 'url, data, raw_data, headers, cookies')
-
-def postForm(url, data, headers={}, cookies={}, auth=None):
- return post(url, urlencode(data).encode('latin1'), headers, cookies, auth)
-
-def postFile(url, fileName, fileData, fieldName='', headers={}, cookies={}, auth=None):
- boundary = ''.join(random.choice('-_' + string.digits + string.ascii_letters) for _ in range(40))
- headers['Content-type'] = 'multipart/form-data; boundary=%s' % boundary
- data = b'\r\n'.join([
-  b'--' + boundary.encode('latin1'),
-  b'Content-Disposition: form-data; name="' + fieldName.encode('latin1') + b'"; filename="' + fileName.encode('latin1') + b'"',
-  b'',
-  fileData,
-  b'--' + boundary.encode('latin1') + b'--',
-  b'',
- ])
- return request(url, data, headers, cookies, auth)
+HttpResponse = namedtuple('HttpResponse', 'url, data, raw_data, headers')
 
 def get(url, data={}, headers={}, cookies={}, auth=None):
  if data:
@@ -56,9 +36,7 @@ def request(url, data=None, headers={}, cookies={}, auth=None):
   # App engine
   pass
  response = build_opener(*handlers).open(request)
- cj = CookieJar()
- cj.extract_cookies(response, request)
  headers = dict(response.headers)
  raw_contents = response.read()
  contents = raw_contents.decode(headers.get('charset', 'latin1'))
- return HttpResponse(urlparse(response.geturl()), contents, raw_contents, headers, dict((c.name, c.value) for c in cj))
+ return HttpResponse(urlparse(response.geturl()), contents, raw_contents, headers)
